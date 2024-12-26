83e4cc928614247879f46860af99e6bb7f1bb21a
Luke David Turnbull <lukedturnbull@gmail.com>
2024-12-27 10:15:58 +1100

    Pikuma - Creating an SDL Window

--- src/main.c
+++ src/main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-#include <stfbool.h>
+#include <stdbool.h>
 #include <SDL2/SDL.h>
 
-SDL_Window* window;
+bool is_running = false;
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
 
 bool initialize_window(void) {
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0 ){
@@ -10,15 +12,32 @@ bool initialize_window(void) {
 		return false;
 	}
 
-   window = SDL_CreateWindow();
+   window = SDL_CreateWindow(
+		NULL,
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		800,
+		600,
+		SDL_WINDOW_BORDERLESS
+	);
+
+	if (!window) {
+		fprintf(stderr, "Error creating SDL window.\n");
+		return false;
+	}
+
+	// Create a SDL renderer
+	renderer = SDL_CreateRenderer(window, -1, 0);
+	if (!renderer) {
+		fprintf(stderr, "Error creating SDL renderer.\n");
+		return false;
+	}
 
 	return true;
 }
 
 int main(void) {
-   
-   /* TODO: Create a SDL window */
-   initialize_window();
+   is_running = initialize_window();
 
    return 0;
 }
